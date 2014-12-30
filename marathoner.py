7888c040704c5fa6036a41471e14295a521941f3
Will <ashlynn.wonderland@gmail.com>
2014-12-30 02:23:43 -0500

    added skipping

--- marathoner.py
+++ marathoner.py
@@ -7,6 +7,21 @@ crunchyroll = MetaApi()
 episodes = crunchyroll.list_media(crunchyroll.search_anime_series(raw_input('Anime name: '))[0])
 length = len(episodes)
 
+def prompt (episodeNumber):
+  print 'Play episode ' + str(episodeNumber + 1) +' (y/n/r/s)?',
+  response = raw_input()
+  if response == 'y':
+    print 'Playing episode ' + str(episodeNumber)
+    playEpisode(episodeNumber + 1)
+  elif response == 'r':
+    print 'Repeating episode ' + str(episodeNumber)
+    playEpisode(episodeNumber)
+  elif response == 's':
+    print 'Skipped.'
+    prompt(episodeNumber + 1)
+  else:
+    print 'Thanks for watching'
+
 def playEpisode(episodeNumber):
   episode = episodes[length-episodeNumber]
   streamlist = streams(episode.url)
@@ -18,13 +33,6 @@ def playEpisode(episodeNumber):
   with open(os.devnull, 'w') as f:
     proc = Popen(['mpv', '--fullscreen', '--cache=4096', streamlist[response].url], stdout=f)
     proc.wait()
-    print 'Play episode ' + str(episodeNumber + 1) +' (y/n/r)?',
-    response = raw_input()
-    if response == 'y':
-      playEpisode(episodeNumber + 1)
-    elif response == 'r':
-      playEpisode(episodeNumber)
-    else:
-      print 'Thanks for watching'
+    prompt(episodeNumber)
     
 playEpisode(int(raw_input('Episode number: ')))
